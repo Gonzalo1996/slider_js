c1c05f2ab60805099d68b8e644ba65775f341de7
Gonzalo1996 <gonald96@gmail.com>
2021-07-16 16:25:36 -0300

    Terminado.
    Slider terminado con solo corregir el li undifinide.

--- js/slider.js
+++ js/slider.js
@@ -1,46 +1,45 @@
-const $btnRigth = document.getElementById('btn-rigth');
-const $btnLeft = document.getElementById('btn-left');
-let $sliders = document.querySelectorAll('.container-slider'); 
-
-let cont = 0;
+const next = document.getElementById('btn-rigth');
+const previous = document.getElementById('btn-left');
+let $sliders = document.querySelectorAll('.slider'); 
 
 
-console.log($sliders)
+
+
+//console.log($sliders)
+
+
+function moveSlide(children,index){
+    
+    for (let i in children) {
+        const  li = children[i];//.style.transform = "translateX(-" + cont + "00%)";   
+        li.style.transform = "translateX(-" + index + "00%)";
+    }
+}
 
 $sliders.forEach(function(slider){
- 
+    let cont = 0;
     const previous = slider.previousElementSibling;
     const next = slider.nextElementSibling;
-/*
+
     console.log("Next", next);
     console.log("After", previous);
     console.log(slider.children.length)
-*/
-    $btnLeft.onclick = function(){
+
+
+    previous.onclick = function(){
         cont --;
         if(cont === -1) 
             cont = slider.children.length -1;
-        console.log(cont)
-        for (let i in slider.children) {
-            const  l = slider.children[i].style.transform = "translateX(-" + cont + "00%)";   
-            //li.style.transform = "translateX(-" + cont + "00%)";
-        }
+        moveSlide(slider.children, cont);
     }
 
-    $btnRigth.onclick = function(){
+    next.onclick = function(){
         cont ++;
         
         if(cont === slider.children.length) 
             cont = 0;
 
-        for (let i in slider.children) {
-            const l = slider.children[i].style.transform = "translateX(-" + cont + "00%)";
-            //li.style.transform = "translateX(-" + cont + "00%)";
-        }
+            moveSlide(slider.children, cont);
     }
 })
-
-setInterval(function(){
-   
-},1000)
 
